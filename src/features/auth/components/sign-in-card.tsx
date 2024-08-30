ae39e01bddfc035e85f029ed5fa4f95c43982046
readul-islam <developer.readul@gmail.com>
2024-08-30 16:34:30 +0600

    added faEye icon for showing password

--- src/features/auth/components/sign-in-card.tsx
+++ src/features/auth/components/sign-in-card.tsx
@@ -1,72 +1,107 @@
-import { FcGoogle } from "react-icons/fc"
-import { FaGithub } from "react-icons/fa"
-import { Button } from "@/components/ui/button"
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
-import { Input } from "@/components/ui/input"
-import { Separator } from "@/components/ui/separator"
-import { SignInFlow } from "../types"
-import { useState } from "react"
-import GoogleAuthButton from "@/components/ui/googleAuthButton"
-import GitHubAuthButton from "@/components/ui/githubAuthButton"
+import { FcGoogle } from "react-icons/fc";
+import { FaGithub } from "react-icons/fa";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Separator } from "@/components/ui/separator";
+import { SignInFlow } from "../types";
+import { useState } from "react";
+import GoogleAuthButton from "@/components/ui/googleAuthButton";
+import GitHubAuthButton from "@/components/ui/githubAuthButton";
+import { useAuthActions } from "@convex-dev/auth/react";
+import { TriangleAlert } from "lucide-react";
 
 interface SignInCardProps {
-    setState: (state: SignInFlow) => void;
-
+  setState: (state: SignInFlow) => void;
 }
 
 export const SignInCard = ({ setState }: SignInCardProps) => {
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
-    const [pending , setPending] = useState<boolean>(false);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const [pending, setPending] = useState<boolean>(false);
+  const { signIn } = useAuthActions();
 
-    const onPassword = (e:React.FormEvent<HTMLFormElement>)=>{
-        e.preventDefault();
-    }
-    return (
-        <Card className="w-full h-full p-8">
-            <CardHeader className="px-0 pt-0">
-                <CardTitle>
-                    Login to continue
-                </CardTitle>
-                <CardDescription>
-                    Use your email or another service to continue
-                </CardDescription>
-            </CardHeader>
-            <CardContent className="space-y-5 px-0 pb-0">
-                <form className="space-y-2.5">
-                    <Input
-                        disabled={pending}
-                        value={email}
-                        onChange={(e) => { setEmail(e.target.value) }}
-                        placeholder="Email"
-                        type="email"
-                        required
-                    />
-                    <Input
-                        disabled={pending}
-                        value={password}
-                        onChange={(e) => { setPassword(e.target.value) }}
-                        placeholder="password"
-                        type="password"
-                        required
-                    />
-                    <Button type="submit" className="w-full" size={"lg"} disabled={pending}>Continue</Button>
-                </form>
-                <Separator />
-                <div className="flex flex-col space-y-2.5">
-                    <GoogleAuthButton pending={pending} setPending={setPending}/>
-                    <GitHubAuthButton pending={pending} setPending={setPending}/>
+  const onPasswordSignIn = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
 
-                    
-                    <p className="text-xs text-muted-foreground">
+    setPending(true);
+    signIn("password", { email, password, flow: "signIn" })
+      .catch(() => {
+        setError("Invalid email or password");
+      })
+      .finally(() => {
+        setPending(false);
+      });
+  };
+  return (
+    <Card className="w-full h-full p-8">
+      <CardHeader className="px-0 pt-0">
+        <CardTitle>Login to continue</CardTitle>
+        <CardDescription>
+          Use your email or another service to continue
+        </CardDescription>
+      </CardHeader>
+      {!!error && (
+        <div className="bg-destructive/15 p-3 rounded-md flex items-center gap-x-2 text-sm text-destructive mb-6">
+          <TriangleAlert className="size-4." />
+          <p>{error}</p>
+        </div>
+      )}
+      <CardContent className="space-y-5 px-0 pb-0">
+        <form onSubmit={onPasswordSignIn} className="space-y-2.5">
+          <Input
+            disabled={pending}
+            value={email}
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+            placeholder="Email"
+            type="email"
+            required
+          />
+          <Input
+            disabled={pending}
+            value={password}
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+            placeholder="password"
+            type="password"
+            required
+          />
+          <Button
+            type="submit"
+            className="w-full"
+            size={"lg"}
+            disabled={pending}
+          >
+            Continue
+          </Button>
+        </form>
+        <Separator />
+        <div className="flex flex-col space-y-2.5">
+          <GoogleAuthButton pending={pending} setPending={setPending} />
+          <GitHubAuthButton pending={pending} setPending={setPending} />
 
-                        Don&apos;t have an account?<span
-                            onClick={() => setState("signUp")}
-                            className="text-sky-700 hover:underline cursor-pointer"> Sign up</span>
-                    </p>
-                </div>
-            </CardContent>
-
-        </Card>
-    )
-}
+          <p className="text-xs text-muted-foreground">
+            Don&apos;t have an account?
+            <span
+              onClick={() => setState("signUp")}
+              className="text-sky-700 hover:underline cursor-pointer"
+            >
+              {" "}
+              Sign up
+            </span>
+          </p>
+        </div>
+      </CardContent>
+    </Card>
+  );
+};
--- src/features/auth/components/sign-up-card.tsx
+++ src/features/auth/components/sign-up-card.tsx
@@ -9,20 +9,44 @@ import {
 import { Input } from "@/components/ui/input";
 import { Separator } from "@/components/ui/separator";
 import { useState } from "react";
-import { FaGithub } from "react-icons/fa";
+import { FaEye, FaEyeSlash, FaGithub } from "react-icons/fa";
 import { FcGoogle } from "react-icons/fc";
 import { SignInFlow } from "../types";
 import GoogleAuthButton from "@/components/ui/googleAuthButton";
 import GitHubAuthButton from "@/components/ui/githubAuthButton";
+import { useAuthActions } from "@convex-dev/auth/react";
+import { TriangleAlert } from "lucide-react";
 
 interface SignUpCardProps {
   setState: (state: SignInFlow) => void;
 }
 export const SignUpCard = ({ setState }: SignUpCardProps) => {
+  const [show, setShow] = useState(false);
+  const [show1, setShow1] = useState(false);
+  const [error, setError] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
-  const [pending , setPending] = useState<boolean>(false);
+  const [pending, setPending] = useState<boolean>(false);
+  const { signIn } = useAuthActions();
+
+  const onPasswordSignUp = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (password !== confirmPassword) {
+      setError("Passwords do not match");
+      return;
+    }
+    setPending(true);
+    signIn("password", { email, password, flow: "signUp" })
+      .catch((e) => {
+        console.log(e);
+        setError("Something went wrong");
+      })
+      .finally(() => {
+        setPending(false);
+      });
+  };
+
   return (
     <Card className="w-full h-full p-8">
       <CardHeader className="px-0 pt-0">
@@ -31,8 +55,14 @@ export const SignUpCard = ({ setState }: SignUpCardProps) => {
           Use your email or another service to continue
         </CardDescription>
       </CardHeader>
+      {!!error && (
+        <div className="bg-destructive/15 p-3 rounded-md flex items-center gap-x-2 text-sm text-destructive mb-6">
+          <TriangleAlert className="size-4." />
+          <p>{error}</p>
+        </div>
+      )}
       <CardContent className="space-y-5 px-0 pb-0">
-        <form className="space-y-2.5">
+        <form onSubmit={onPasswordSignUp} className="space-y-2.5">
           <Input
             disabled={pending}
             value={email}
@@ -43,16 +73,27 @@ export const SignUpCard = ({ setState }: SignUpCardProps) => {
             type="email"
             required
           />
-          <Input
-            disabled={pending}
-            value={password}
-            onChange={(e) => {
-              setPassword(e.target.value);
-            }}
-            placeholder="password"
-            type="password"
-            required
-          />
+          <div className="relative">
+            <Input
+              disabled={pending}
+              value={password}
+              onChange={(e) => {
+                setPassword(e.target.value);
+              }}
+              placeholder="password"
+              type={show ? "text" : "password"}
+              required
+            />
+            <div className="absolute top-3 text-slate-500 right-3 cursor-pointer">
+              {show ? (
+                <FaEye onClick={() => setShow(false)} />
+              ) : (
+                <FaEyeSlash onClick={() => setShow(true)} />
+              )}
+             
+            </div>
+          </div>
+          <div className="relative">
           <Input
             disabled={pending}
             value={confirmPassword}
@@ -60,10 +101,25 @@ export const SignUpCard = ({ setState }: SignUpCardProps) => {
               setConfirmPassword(e.target.value);
             }}
             placeholder="Confirm password"
-            type="password"
+            type={show1?"text":"password"}
             required
           />
-          <Button type="submit" className="w-full" size={"lg"} disabled={pending}>
+          <div className="absolute top-3 text-slate-500 right-3 cursor-pointer">
+              {show1 ? (
+                <FaEye onClick={() => setShow1(false)} />
+              ) : (
+                <FaEyeSlash onClick={() => setShow1(true)} />
+              )}
+             
+            </div>
+          </div>
+         
+          <Button
+            type="submit"
+            className="w-full"
+            size={"lg"}
+            disabled={pending}
+          >
             Continue
           </Button>
         </form>
