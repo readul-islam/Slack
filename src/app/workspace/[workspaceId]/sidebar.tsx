2a674c540d7349c5c676b7ddad60e6f273e90a77
readul-islam <developer.readul@gmail.com>
2024-09-01 21:09:37 +0600

    add workspace sidebar

--- src/app/workspace/[workspaceId]/sidebar.tsx
+++ src/app/workspace/[workspaceId]/sidebar.tsx
@@ -1,11 +1,18 @@
 import UserButton from "@/features/auth/components/user-button";
+import { Bell, Home, MessageSquare, MoreHorizontal } from "lucide-react";
+import SidebarButton from "./sidebar-button";
 import WorkspaceSwitcher from "./workspace-switcher";
+import { usePathname } from "next/navigation";
 
 const Sidebar = () => {
+  const pathname = usePathname();
   return (
     <aside className="bg-[#481349] w-[70px] h-full flex flex-col gap-y-4 items-center pt-[9px] pb-4">
-
-      <WorkspaceSwitcher/>
+      <WorkspaceSwitcher />
+      <SidebarButton icon={Home} label="Home" isActive={pathname.includes('/workspace')} />
+      <SidebarButton icon={MessageSquare} label="DMs" isActive={pathname.includes('/d')}  />
+      <SidebarButton icon={Bell} label="Activity" isActive={pathname.includes('/d')}  />
+      <SidebarButton icon={MoreHorizontal} label="More" isActive={pathname.includes('/d')}  />
       <div className="flex flex-col items-center justify-center gap-y-1 mt-auto">
         <UserButton />
       </div>
--- src/app/workspace/[workspaceId]/workspace-switcher.tsx
+++ src/app/workspace/[workspaceId]/workspace-switcher.tsx
@@ -10,7 +10,7 @@ import { useGetWorkspaces } from "@/features/workspaces/api/use-get-workspaces";
 import { useCreateWorkspaceModal } from "@/features/workspaces/store/use-create-workspace-modal";
 import { useWorkspaceId } from "@/hooks/create-workspace-id";
 
-import { Loader } from "lucide-react";
+import { Loader, Plus, PlusIcon } from "lucide-react";
 import { useRouter } from "next/navigation";
 
 const WorkspaceSwitcher = () => {
@@ -29,7 +29,7 @@ const WorkspaceSwitcher = () => {
   );
   return (
     <DropdownMenu>
-      <DropdownMenuTrigger>
+      <DropdownMenuTrigger asChild>
         <Button className="size-9 relative overflow-hidden bg-[#ABABAD] hover:bg-[#ABABAD]/80 text-slate-800 font-semibold text-xl">
           {workspaceLoading ? (
             <Loader className="size-5 animate-spin shrink-0" />
@@ -39,16 +39,39 @@ const WorkspaceSwitcher = () => {
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent side="bottom" align="start" className="w-64">
-        <DropdownMenuItem 
-        onClick={()=>router.push(`/workspace/${workspaceId}`)}
-        className="cursor-pointer  flex-col  items-start capitalize">
+        <DropdownMenuItem
+          onClick={() => router.push(`/workspace/${workspaceId}`)}
+          className="cursor-pointer  flex-col  items-start capitalize"
+        >
           {workspace?.name}
           <span className=" text-xs text-muted-foreground">
             Active workspace
           </span>
         </DropdownMenuItem>
+        {filteredWorkspace?.map((workspace) => (
+          <DropdownMenuItem
+            className="cursor-pointer capitalize overflow-hidden"
+            key={workspace?._id}
+            onClick={() => router.push(`/workspace/${workspace._id}`)}
+          >
+
+          <div className="shrink-0   size-9 relative overflow-hidden bg-[#616061] text-white text-lg rounded-md flex items-center justify-center mr-2">
+          {workspace.name.charAt(0).toUpperCase()}
+          </div>
+          <p className="truncate">{workspace.name}</p>
+
+
+          </DropdownMenuItem>
+        ))}
+
+        <DropdownMenuItem onClick={()=> setOpen(true)} className="cursor-pointer">
+          <div className="size-9 relative overflow-hidden bg-[#F2F2F2] text-slate-800 text-lg rounded-md flex items-center justify-center mr-2">
+            <Plus/>
+          </div>
+            Create a new workspace
+        </DropdownMenuItem>
       </DropdownMenuContent>
-    </DropdownMenu> 
+    </DropdownMenu>
   );
 };
 
