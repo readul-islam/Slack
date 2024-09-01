028f94d643f6ca10b7743815f0c13ff34da3d8d7
readul-islam <developer.readul@gmail.com>
2024-09-01 13:23:50 +0600

    update

--- src/app/workspace/[workspaceId]/workspace-switcher.tsx
+++ src/app/workspace/[workspaceId]/workspace-switcher.tsx
@@ -1,11 +1,22 @@
 import { Button } from "@/components/ui/button";
-import { DropdownMenu, DropdownMenuContent, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
+import {
+  DropdownMenuItem,
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 import { useGetWorkspace } from "@/features/workspaces/api/use-get-workspace";
 import { useGetWorkspaces } from "@/features/workspaces/api/use-get-workspaces";
+import { useCreateWorkspaceModal } from "@/features/workspaces/store/use-create-workspace-modal";
 import { useWorkspaceId } from "@/hooks/create-workspace-id";
 
+import { Loader } from "lucide-react";
+import { useRouter } from "next/navigation";
+
 const WorkspaceSwitcher = () => {
+  const router = useRouter();
   const workspaceId = useWorkspaceId();
+  const [_open, setOpen] = useCreateWorkspaceModal();
 
   const { data: workspaces, isLoading: workspacesLoading } = useGetWorkspaces();
 
@@ -16,14 +27,29 @@ const WorkspaceSwitcher = () => {
   const filteredWorkspace = workspaces?.filter(
     (workspace) => workspace?._id !== workspaceId
   );
-  return <DropdownMenu>
-    <DropdownMenuTrigger>
-        <Button className="size-9 relative overflow-hidden bg-[#ABABAD] hover:bg-[#ABABAD]/80 text-slate-800 font-semibold text-xl">A</Button>
-    </DropdownMenuTrigger>
-    <DropdownMenuContent side="bottom" align="start" className="w-64">
-
-    </DropdownMenuContent>
-  </DropdownMenu>;
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger>
+        <Button className="size-9 relative overflow-hidden bg-[#ABABAD] hover:bg-[#ABABAD]/80 text-slate-800 font-semibold text-xl">
+          {workspaceLoading ? (
+            <Loader className="size-5 animate-spin shrink-0" />
+          ) : (
+            workspace?.name.charAt(0).toUpperCase()
+          )}
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent side="bottom" align="start" className="w-64">
+        <DropdownMenuItem 
+        onClick={()=>router.push(`/workspace/${workspaceId}`)}
+        className="cursor-pointer  flex-col  items-start capitalize">
+          {workspace?.name}
+          <span className=" text-xs text-muted-foreground">
+            Active workspace
+          </span>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu> 
+  );
 };
 
 export default WorkspaceSwitcher;
