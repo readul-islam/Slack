bda9491514bd7bb69aceced31d99abe415522f41
readul-islam <developer.readul@gmail.com>
2024-09-07 20:51:55 +0600

    qery member of specific workspace

--- convex/members.ts
+++ convex/members.ts
@@ -1,6 +1,32 @@
 import { v } from "convex/values";
-import { mutation, query } from "./_generated/server";
+import { mutation, query, QueryCtx } from "./_generated/server";
 import { getAuthUserId } from "@convex-dev/auth/server";
+import { Id } from "./_generated/dataModel";
+
+
+const populateUser = (ctx:QueryCtx, id:Id<"users">)=>{
+  return ctx.db.get(id);
+}
+
+export const get = query({
+  args:{workspaceId:v.id("workspaces")},
+  handler: async (ctx, args) => {
+    const userId = await getAuthUserId(ctx);
+    if (!userId) return [];
+    const member = await ctx.db
+      .query("members")
+      .withIndex("by_workspace_id_user_id", (q) =>
+        q.eq("workspaceId", args.workspaceId).eq("userId", userId)
+      )
+      .unique();
+    if (!member) return [];
+    const data = await ctx.db.query("members").withIndex("by_workspace_id", (q)=> q.eq("workspaceId", args.workspaceId)).collect();
+
+    return member;
+  },
+})
+
+
 
 export const current = query({
   args: { workspaceId: v.id("workspaces") },
@@ -17,3 +43,5 @@ export const current = query({
     return member;
   },
 });
+
+
